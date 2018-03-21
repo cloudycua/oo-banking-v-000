7f9888f05179607bc05aa28b7f6212c3591fe757
Christine Cua <cloudycua@gmail.com>
2018-03-21 21:38:18 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
 
   def execute_transaction
-    if count < 1 && sender.valid?
+    if status == "pending" && self.valid?
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
       @status = "complete"
