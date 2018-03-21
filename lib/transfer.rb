f0e66d8eb7760c20adfbbdf045407662ae6f8923
Christine Cua <cloudycua@gmail.com>
2018-03-21 21:32:03 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,6 +17,7 @@ class Transfer
   end
 
   def execute_transaction
+    binding.pry
     if count < 1 && sender.valid?
       sender.balance = sender.balance - amount
       receiver.balance = receiver.balance + amount
